ffc5a55810253c304927f02b5a18d7e999a92fbb
Gabriel Meira <88985229+gabriel-meirax@users.noreply.github.com>
2025-07-06 16:22:10 +0000

    modifica o conteúdo da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é capaz de realizar tudo que deseja, basta ter força e vontade!!!</h1>
+    return <h1>Você é capaz de realizar tudo que deseja, basta ter força e vontade!!! Lembre-se também de quem estava junto com você nos diversos momentos, tanto nos bons, como nos ruins.</h1>
 }
 
 export default Home;
