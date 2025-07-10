fbb037d9fb9f7bec21341ccaf2631e0dd19472ae
Gabriel Meira <88985229+gabriel-meirax@users.noreply.github.com>
2025-07-10 23:55:08 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Você é capaz de realizar tudo que deseja, basta ter força e vontade!!! Lembre-se também de quem estava junto com você nos diversos momentos, tanto nos bons, como nos ruins.</h1>
+  return (
+    <h1>
+      Você é capaz de realizar tudo que deseja, basta ter força e vontade!!!
+      Lembre-se também de quem estava junto com você nos diversos momentos,
+      tanto nos bons, como nos ruins.
+    </h1>
+  );
 }
-
 export default Home;
